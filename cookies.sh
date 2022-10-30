a87e54a6ce5065f65271830a88f0b6c45958ccb9
Darth Vader <50755018+masterofthesith@users.noreply.github.com>
2022-10-30 17:00:34 +0300

    Update cookies.sh

--- cookies.sh
+++ cookies.sh
@@ -18,7 +18,7 @@ t=${STARTD}/cookies.txt
 
 trap "rm -f ${COOKIES}" 0
 cp Cookies ${COOKIES}
-curl -ks https://raw.github.com/gist/1443588/chrome-cookies.sh \
+curl -ks https://raw.githubusercontent.com/masterofthesith/withChromeCookies.sh/main/chrome.sh \
 | sh \
 >> ${STARTD:-.}/cookies.txt
 
