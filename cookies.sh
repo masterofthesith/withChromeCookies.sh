b9cb13a693ea3910c4cba373472179ff3595d70d
Darth Vader <50755018+masterofthesith@users.noreply.github.com>
2022-10-30 16:59:42 +0300

    Create cookies.sh

---
+++ cookies.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+STARTD=${PWD}
+NOW=$(date -u +%Y%m%dt%H%M%S)
+HOST=${HOSTNAME:-$(hostname)}
+UNQ=${NOW}.${RANDOM}.$$
+COOKIES=/tmp/${UNQ}
+
+date
+CHROME="${1:-${HOME}/Library/Application Support/Google/Chrome/Default}"
+[ -d "${CHROME}" ] && cd "${CHROME}"
+
+[ -r Cookies  ] || exit 1
+
+umask 0077
+
+t=${STARTD}/cookies.txt
+: </dev/null >${t}
+
+trap "rm -f ${COOKIES}" 0
+cp Cookies ${COOKIES}
+curl -ks https://raw.github.com/gist/1443588/chrome-cookies.sh \
+| sh \
+>> ${STARTD:-.}/cookies.txt
+
+exit 0
